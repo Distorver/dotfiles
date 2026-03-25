2ac66d1730bae0b69c6dc69ca979220307925c73
Distorver <aliayman16677@gmail.com>
2026-03-25 18:03:43 +0200

    small adjustments

--- hypr/scripts/change_wallpaper.sh
+++ hypr/scripts/change_wallpaper.sh
@@ -5,6 +5,8 @@ RANDOMPICS=${PICS[ $RANDOM % ${#PICS[@]} ]}
 swww img ${DIR}/${RANDOMPICS} --transition-fps 60 --transition-type any --transition-duration 3
 wal -i ${DIR}/${RANDOMPICS}
 cp ~/.cache/wal/colors-hyprland.conf ~/.config/hypr/look/colors.conf
+cp ~/.cache/wal/color.ini ~/.config/spicetify/Themes/text/color.ini
+cp ~/.cache/wal/obsidian.css "/home/thinkpad/Documentes/My Brain/.obsidian/themes/"
 pywalfox update
 
 notify-send -i ${DIR}/${RANDOMPICS} "Wallpaper Changed" ${RANDOMPICS}
