cc883255496050272ca7def9aff0157dd6e5e701
Distorver <aliayman16677@gmail.com>
2026-04-03 05:40:17 +0200

    added hyprlock and hypridle

--- hypr/scripts/change_wallpaper.sh
+++ hypr/scripts/change_wallpaper.sh
@@ -2,11 +2,12 @@ DIR=~/Pictures/wallpapers/
 PICS=($(ls ${DIR}))
 
 RANDOMPICS=${PICS[ $RANDOM % ${#PICS[@]} ]}
-awww img ${DIR}/${RANDOMPICS} --transition-fps 60 --transition-type any --transition-duration 3
+awww img ${DIR}/${RANDOMPICS} --transition-fps 60 --transition-type any --transition-duration 2
 wal -i ${DIR}/${RANDOMPICS}
 cp ~/.cache/wal/colors-hyprland.conf ~/.config/hypr/look/colors.conf
 cp ~/.cache/wal/color.ini ~/.config/spicetify/Themes/text/color.ini
 cp ~/.cache/wal/obsidian.css "/home/thinkpad/Documentes/My Brain/.obsidian/themes/"
+ln -sf ${DIR}/${RANDOMPICS} ~/.current_wallpaper.png
 pywalfox update
 
 notify-send -i ${DIR}/${RANDOMPICS} "Wallpaper Changed" ${RANDOMPICS}
